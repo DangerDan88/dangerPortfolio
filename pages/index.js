be7c8bb2dc6e322d8a0b0b6972a0e8a254dc5439
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-11-30 22:44:56 -0700

    playing around with react spring for personalsite

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Navbar from "./components/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,105 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <div className="flex justify-center">
-        <Image
-          src="/images/profilepic.jpeg"
-          alt="me"
-          className="rounded-full"
-          height="225"
-          width="225"
-        />
-      </div>
-      <main className="flex justify-center text-white">
-        <p className="py-6 text-lg text-black">
-          I'm Daniel I am a freelance full stack web developer. I am currently
-          available for work please contact me if interested below or take a
-          look at my current completed projects.
-        </p>
-      </main>
-      <div className="flex justify-center mx-auto pt-6">
-        <form
-          className="w-full max-w-lg"
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <div className="flex flex-wrap -mx-3 mb-6 pt-8">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
-                htmlFor="grid-first-name"
-              >
-                First Name
-                <input type="hidden" name="bot-field" />
-                <input type="hidden" name="form-name" value="contact" />
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-white border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                  id="grid-first-name"
-                  type="text"
-                  name="name"
-                  placeholder="Jane"
-                />
-              </label>
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-              <label
-                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
-                htmlFor="grid-last-name"
-              >
-                Last Name
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-white border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-last-name"
-                  type="text"
-                  placeholder="Doe"
-                />
-              </label>
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
-                htmlFor="grid-password"
-              >
-                E-mail
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="email"
-                  type="email"
-                />
-              </label>
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label
-                className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
-                htmlFor="grid-password"
-              >
-                Message
-              </label>
-              <textarea
-                className=" no-resize appearance-none block w-full bg-gray-200 text-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-                id="message"
-              ></textarea>
-            </div>
-          </div>
-          <div className="md:flex md:items-center">
-            <div className="md:w-1/3">
-              <button
-                className="rounded-lg bg-white text-black font-bold py-2 px-6 hover:bg-indigo-300 shadow-xl border-b-4 border-blue-700 hover:border-blue-500"
-                type="submit"
-              >
-                Send
-              </button>
-            </div>
-            <div className="md:w-2/3"></div>
-          </div>
-        </form>
-      </div>
+      <div className="flex justify-center"></div>
+      <main className="flex justify-center"></main>
+      <div className="flex justify-center mx-auto pt-6"></div>
     </div>
   );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Navbar from "./components/Navbar";
-import Image from "next/image";
+import Card from "./components/Card";
 
 const Projects = () => {
   return (
@@ -13,77 +13,16 @@ const Projects = () => {
       <Navbar />
       <h1 className="flex justify-center text-2xl font-bold">Projects</h1>
       <main id="grid" className="py-8">
-        <div className="max-w-sm lg:max-w-full rounded bg-white overflow-hidden shadow-lg">
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">LearnBuildType</div>
-            <Image
-              className="w-full"
-              src="/images/learnbuild.png"
-              alt="LBT game"
-              width="400"
-              height="400"
-            ></Image>
-            <p className="text-gray-700 text-base">Info here</p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #serverless
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #React
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #Auth0
-            </span>
-          </div>
-        </div>{" "}
-        <div className="max-w-sm rounded bg-white overflow-hidden shadow-lg">
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">GPH Equestrian</div>
-            <Image
-              className="w-full"
-              src="/images/gphProjectPic.png"
-              alt="LBT game"
-              width="400"
-              height="400"
-            />
-            <p className="text-gray-700 text-base">Info here</p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #serverless
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #React
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #Auth0
-            </span>
-          </div>
+        <div className="gridBox bg-white rounded-lg flex justify-center">
+          <a href="https://gphequestrian.com/" target="_blank">
+            <Card />
+          </a>
         </div>
-        <div className="max-w-sm rounded bg-white overflow-hidden shadow-lg">
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Food Memory 101</div>
-            <Image
-              className="w-full"
-              src="/images/foodMemory.png"
-              alt="LBT game"
-              width="400"
-              height="400"
-            />
-            <p className="text-gray-700 text-base">Info here</p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #serverless
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #React
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #Auth0
-            </span>
-          </div>
+        <div className="gridBox  bg-white rounded-lg flex justify-center">
+          LearnBuildType
+        </div>
+        <div className="gridBox bg-white rounded-lg flex justify-center">
+          Food Memory
         </div>
       </main>
     </div>
